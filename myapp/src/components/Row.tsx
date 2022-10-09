37596ee8b4a7ff586cea7d764e9531568ab23d85
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-10-10 04:03:21 +0900

    check api

---
+++ myapp/src/components/Row.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+type Props = {
+  title: string;
+  fetchUrl: string;
+  isLargeRow?: boolean;
+};
+
+type Movie = {
+  id: string;
+  name: string;
+  title: string;
+  original_name: string;
+  poster_path: string;
+  backdrop_path: string;
+};
+
+export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+  return (
+    <div className="Row">
+      <h2>{title}</h2>
+      {movies.map((movie, i) => (
+        <img
+          width="70"
+          key={movie.id}
+          src={`${base_url}${movie.poster_path}`}
+          alt={movie.name}
+        />
+      ))}
+    </div>
+  );
+};
